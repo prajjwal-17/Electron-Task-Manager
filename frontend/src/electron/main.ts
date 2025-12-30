caeeba0989a5d221c89965d1fc751b7c96c4b157
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-31 03:31:13 +0530

    TypeScript added

--- frontend/src/electron/main.ts
+++ frontend/src/electron/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
+type test = string;
+
 app.on("ready",()=>{
     const mainWindow = new BrowserWindow({});
     mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html')) // this will get current app path(where app is running from)
