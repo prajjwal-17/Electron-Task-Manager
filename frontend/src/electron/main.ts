f0c6a399cf9eb9cdf0f651f50ac32de982c50e05
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-31 06:48:26 +0530

    DX(dev experience) improvements

--- frontend/src/electron/main.ts
+++ frontend/src/electron/main.ts
@@ -1,10 +1,16 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { isDev } from './util.js';
 
 type test = string;
 
 app.on("ready",()=>{
     const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html')) // this will get current app path(where app is running from)
+    if(isDev()){
+        mainWindow.loadURL('http://localhost:5123');
+    }
+    else{
+          mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html')) // this will get current app path(where app is running from)
+    }
 
 })
