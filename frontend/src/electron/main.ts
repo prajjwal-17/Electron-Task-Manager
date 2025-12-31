a256a54987ab5055a12495dbaa5cf9bd49332255
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-12-31 07:42:59 +0530

    Added all resource functions

--- frontend/src/electron/main.ts
+++ frontend/src/electron/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import { isDev } from './util.js';
+import { pollResources } from './resourceManager.js';
 
 type test = string;
 
@@ -12,5 +13,7 @@ app.on("ready",()=>{
     else{
           mainWindow.loadFile(path.join(app.getAppPath(),'/dist-react/index.html')) // this will get current app path(where app is running from)
     }
+
+    pollResources()
 
 })
---
+++ frontend/src/electron/resourceManager.ts
@@ -0,0 +1,53 @@
+import osUtils from 'os-utils'
+import fs from 'fs'
+import os from 'os'
+
+const POLLING_INTERVAL=500
+
+export function pollResources(){
+     setInterval(async()=>{
+     const cpuUsage =  await getCpuUsage();
+     const ramUsage= getRamUsage();
+     const storageData = getStorageData()
+     console.log({cpuUsage , ramUsage , storageUsage : storageData.usage});
+
+     //static
+     const obj =getStaticData();
+     console.log(obj);
+     },POLLING_INTERVAL);
+}
+
+function getCpuUsage(){
+    return new Promise(resolve => {
+        osUtils.cpuUsage(resolve)
+    })
+    // osUtils.cpuUsage((percentage)=>console.log(percentage*100));
+}
+
+function getRamUsage(){
+    return 1-osUtils.freememPercentage();
+}
+
+function getStorageData(){
+    const stats = fs.statfsSync(process.platform === 'win32'?'C://':'/'); // if windows check c drive else root of filesystem
+    const total = stats.bsize* stats.blocks;// number of blocks * size in bytes
+    const free = stats.bsize * stats.bfree; // size of each block * number of blocks
+
+    return {
+        total : Math.floor(total / 1_000_000_000), //divides by this gives gb
+        usage : 1 - free / total  // percentage of free/total
+    }
+}
+
+export function getStaticData(){
+     const totalStorage = getStorageData().total;
+     const cpuModel = os.cpus()[0].model;
+     const totalMemoryGB = Math.floor (osUtils.totalmem()/1024);
+
+
+     return {
+        totalStorage , 
+        cpuModel , 
+        totalMemoryGB ,
+     }
+}
